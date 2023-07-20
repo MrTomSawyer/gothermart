7c5366cf842da56c4e4848c12be6160c9332e70e
Slava V <leterthing@gmail.com>
2023-07-20 23:15:23 +0700

    order id => number type string

--- internal/app/models/order.go
+++ internal/app/models/order.go
@@ -1,7 +1,7 @@
 package models
 
 type Order struct {
-	Number    int     `json:"number"`
+	Number    string  `json:"number"`
 	UserID    string  `json:"user_id"`
 	OrderID   string  `json:"order_id"`
 	Accrual   float32 `json:"accrual"`
