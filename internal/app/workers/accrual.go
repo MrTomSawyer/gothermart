b6c4477b878b10cc0e44c18a1a641da101dc3113
Slava V <leterthing@gmail.com>
2023-07-27 21:49:37 +0700

    review fixes #2

--- internal/app/workers/accrual.go
+++ internal/app/workers/accrual.go
@@ -31,11 +31,16 @@ func HandleOrders(ctx context.Context, pool *pgxpool.Pool, orderCh chan string, 
 			}()
 
 			// проверяем есть ли такой ордер в базе
-			row := pool.QueryRow(ctx, "SELECT user_id from orders WHERE order_num=$1", orderID)
+			row := pool.QueryRow(ctx, "SELECT user_id, order_status from orders WHERE order_num=$1", orderID)
+			var orderStatus string
 			var userID int
 			err := row.Scan(&userID)
 			if err != nil {
-				logger.Log.Errorf("failed to scan data for order %v", err)
+				logger.Log.Errorf("no order with id=%d found: %v", orderID, err)
+				return
+			}
+			if orderStatus == "INVALID" || orderStatus == "PROCESSED" {
+				logger.Log.Errorf("order with id=%d has status of %s and can't be processed", orderID, orderStatus)
 				return
 			}
 
